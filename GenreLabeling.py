c87014cb55afa627ed88d1c39727233d84292698
Patrick Duane <patrick.duane@student.nmt.edu>
2023-11-15 13:02:26 -0700

    Full dataset downloaded, cleaned up some unnecessary files

--- GenreLabeling.py
+++ GenreLabeling.py
@@ -2,12 +2,17 @@ import os
 from glob import glob
 from shutil import copyfile
 
+# Use a dictionary to keep track of the count of an entry
+# (e.g. tracking the occurances of a word)
 def dictCount(dict, k):
     if k in dict.keys():
         dict[k] += 1
     else:
         dict[k] = 1
 
+# Returns an array of the n keys with the highest count values
+# The dictionary should be of the format
+#        {key:count}
 def nMostCommon(n, dict):
     common = [['', 0]] * n
     for k in dict.keys():
@@ -19,42 +24,55 @@ def nMostCommon(n, dict):
                 break
     return common
 
-root_dir = "prelim_processed"
-ds_dir = "dataset_labeled"
+# The directory to read from
+root_dir = "mss_preview_dataset"
+# The directory to write the labeled dataset to
+ds_dir = "full_dataset_labeled"
 
 genres = {}
 words = {}
 
+# Stage 1: Identify the most common genre superclasses by the number of
+#     occurrances of each word in the dataset genres
 for root, dirs, files in os.walk(root_dir):
     for f in files:
+        # Open the label file (has the list of genres)
         if f.endswith(".lab"):
-            # fname_root = f[:-3]
-            # label_path = os.path.join(root, "{}.lab".format(fname_root))
             label_path = os.path.join(root, f)
             print(label_path)
             lab = open(label_path, 'r')
+            # Each line has 1 genre specific to that artist
             genre = lab.readline()
             while genre != '':
-                dictCount(genres, genre.strip())
+                # dictCount(genres, genre.strip())
                 ws = genre.strip().split(' ')
+                # Subgenres usually have the supergenre in the name, so
+                #     the most common word will probably be the most common
+                #     supergenre
                 for w in ws:
                     w = w.strip()
                     dictCount(words, w)
-                # if genre.strip() in genres.keys():
-                #     genres[genre.strip()] += 1
-                # else:
-                #     genres[genre.strip()] = 1
                 genre = lab.readline()
             lab.close()
 
 print("")
 
+# Print out the 15 most common words
 cmn = nMostCommon(15, words)
 for l in cmn:
     print("{} --> {}".format(l[0], l[1]))
 
-master_genres = ['rock', 'metal', 'pop', 'blues', 'country', 'classic', 'alternative', 'hip hop', 'punk', 'reggae', 'folk', 'jazz']
+# !!! IMPORTANT !!!
+# Place a debug breakpoint below to see the most common words/genres.
+# NOTE: If you do not stop/pause execution here, the label file WILL get overwritten, and you
+#     will have to start over.
+#     YOU MAY WANT TO RUN THIS ON A COPY OF THE DATASET RATHER THAN THE ORIGINAL
+# -------------------------------------------------------------------------------------------
 
+# Stage 2: Labeling by supergenre in a new dataset structure
+master_genres = ['rock', 'metal', 'pop', 'blues', 'country', 'classic', 'hip hop', 'alternative', 'punk', 'reggae', 'folk', 'jazz']
+
+# Create a folder for each genre
 for g in master_genres:
     pathname = "{}\\{}".format(ds_dir, g)
     if (not os.path.exists(pathname)):
@@ -63,14 +81,26 @@ for g in master_genres:
 genre_count = {}
 valid_files = 0
 
+# For each track, count up the occurrances of each supergenre name in
+#     the label file. The supergenre that has the most occurrances in
+#     the file (or the first one found in case of a tie) is taken to
+#     be the label for that track
+
+# Walk through the entire dataset
 for root, dirs, files in os.walk(root_dir):
     for f in files:
+        # Read the label file with the specific genres
         if f.endswith(".lab"):
             label_path = os.path.join(root, f)
             lab = open(label_path, 'r')
 
+            # Initialize the count for each genre to be 0
             for g in master_genres:
                 genre_count[g] = 0
+            
+            # Each line contains one genre the artist belongs to.
+            # If a line contains the name of a supergenre, add one to
+            # the count for that genre
             genre = lab.readline()
             while genre != '':
                 dictCount(genres, genre.strip())
@@ -82,32 +112,22 @@ for root, dirs, files in os.walk(root_dir):
             
             lab.close()
 
+            # Find the genre with the highest number of occurances in the
+            #     label file
             genre = [master_genres[0], genre_count[master_genres[0]]]
             for i in range(1, len(master_genres)):
                 if genre_count[master_genres[i]] > genre[1]:
                     genre = [master_genres[i], genre_count[master_genres[i]]]
             
+            # Copy the audio preview file to the new dataset folder
             fname_root = f[0:-4]
             path_root = os.path.join(root, fname_root)
             fs = glob("{}*".format(path_root))
             if (genre[1] > 0):
                 nfn = "{}\\{}\\{}".format(ds_dir, genre[0], "{}.mp3".format(fname_root))
                 copyfile("{}.mp3".format(path_root), nfn)
-                # print("{} --> {}".format(fname_root, genre[0]))
-                # for fil in fs:
-                #     nfn = "{}\\{}\\{}".format(ds_dir, genre[0], os.path.basename(fil))
-                #     copyfile(fil, nfn)
-                # with open(label_path, "w") as out:
-                #     out.write(genre[0])
-                #     out.close()
                 print("{} --> {}".format(label_path, genre[0]))
                 valid_files += 1
-            # else:
-            #     # fname_root = f[0:-4]
-            #     # path_root = os.path.join(root, fname_root)
-            #     # fs = glob("{}*".format(path_root))
-            #     for fil in fs:
-            #         os.remove(fil)
 
 
 print("{} valid files".format(valid_files))
--- process_mp3.py
+++ process_mp3.py
@@ -1,3 +1,7 @@
+# This program converts .mp3 audio preview to .wav files (to be
+#     compatible with the Python library), then generates the
+#     spectral image using the FFT
+
 import simplejson as json
 import requests
 import time
@@ -11,18 +15,42 @@ from multiprocessing import Process, Lock, Array, Value
 import random
 import ctypes
 
+# FFT size should be a power of two (256, 512, 1024, etc)
+# The frequency resolution (pixels in the y-dimension) will be
+#     half of fft_size (half of the values are redundant for
+#     real-valued samples
 fft_size = 512
+
+# The number of pixels in the x direction
 img_len = 256
+
+# The number of samples to average for a single pixel in time
+#     The audio has a sample rate of 44,100 Hz, which is much
+#     higher than we need or want to deal with
+# Averaging 882 samples into a single time step gives us
+#     44,100 / 882 = 50 Hz,
+#     or 1 sample for every .02 seconds. This is consistent with
+#     the pictures generated in the Music-CRN paper
 batch_size = 882
 delay = 0.25
 
+# We are using multiple threads to generate the spectral images
+#     in order to speed up processing (this process takes a few
+#     minutes per song)
+# Change this number based on how many threads you want to use
 num_parallel_procs = 8
 
+# Keep track of what files still need to be processed
 to_process = []
+# Mutex lock to prevent parallel memory access issues
 mutex = Lock()
 
+# The process that acually generates the spectral images.
+#     Each thread is running an instance of this function
 def imageProcess(queue, counter, fft_size, img_len, batch_size):
     while (True):
+        # Exit the loop (and terminate the thread) if there are no
+        #     new files to be processed
         with counter.get_lock():
             if len(queue) <= counter.value:
                 break
@@ -31,14 +59,17 @@ def imageProcess(queue, counter, fft_size, img_len, batch_size):
         print("Generating images for {fname}".format(fname=wav_name))
         audio2spectroimg.audio2images(wav_name, fft_size, img_len, batch_size)
 
+# Make sure spawned threads/processes don't try to run the main function
 if __name__ == '__main__':
-    root_dir = "dataset_labeled"
+    root_dir = "full_dataset_labeled"
     count = 0
 
     for root, dirs, files in os.walk(root_dir):
         for f in files:
             if f.endswith(".wav") or f.endswith(".mp3"):
                 new_path = "{root_dir}\\{tree}".format(root_dir=root_dir, tree=root[len(root_dir) + 1:])
+                
+                # If the file ends with .mp3, convert it to a .wav first
                 if (f.endswith(".mp3")):
                     mp3_name = os.path.join(new_path, f)
                     wav_name = os.path.join(new_path, "{}.wav".format(f[:-4]))
@@ -48,20 +79,20 @@ if __name__ == '__main__':
                 else:
                     wav_name = os.path.join(new_path, f)
                 
+                # Add the wave file to the processing queue
                 mutex.acquire()
                 print("Adding {} to processing queue".format(wav_name))
                 to_process.append(wav_name)
                 mutex.release()
 
-                # count = count + 1
-
     print("Files found: {}".format(count))
     print("Waiting for image processes to finish")
 
+    # idx keeps track of where we are in the processing queue. Easier
+    #     than trying to remove files from the queue
     idx = Value(ctypes.c_int, 0)
-    # for i in range(0,len(to_process)):
-    #     arr[i] = to_process[i].encode('utf-8')
 
+    # Spawn subprocesses
     procs = []
     for i in range(0,num_parallel_procs):
         print("Spawning process {num}".format(num=i + 1))
@@ -69,6 +100,7 @@ if __name__ == '__main__':
         procs.append(p)
         p.start()
 
+    # After all the subprocesses have been spawned, wait for them to finish
     for p in procs:
         p.join()
     
